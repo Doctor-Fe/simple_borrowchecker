606f1fab55f9c3700a44e5df45a0054c5711cc36
Doctor-Fe <sota.m8g1y3+git@gmail.com>
2023-01-27 15:41:08 +0900

    変数に型を追加
    追加
    ・VarType 列挙型
    ・インタプリタ内で使用可能な要素に文字列を追加。
    ・インタプリタ内で使用可能な要素にポインタを追加。(不完全)
    ・未初期化と Void を表す列挙型
    修正
    ・VarType を用いた処理に変更
    ・一時的に複合代入演算子を停止

--- src/parser.rs
+++ src/parser.rs
@@ -13,7 +13,7 @@ use crate::vec_deque;
 /// 式を解釈するパーサです。現時点ではインタプリタとしてのみ動作します。
 pub struct ExprParser {
     cmds: Vec<String>,
-    variables: HashMap<String, i32>,
+    variables: HashMap<String, VarType>,
 }
 
 impl ExprParser {
@@ -35,25 +35,25 @@ impl ExprParser {
 
     /// 可変な状態で変数を取得します。
     /// - `name` - 変数名
-    pub fn get_variable_mut(&mut self, name: &String) -> Option<&mut i32> {
+    pub fn get_variable_mut(&mut self, name: &String) -> Option<&mut VarType> {
         self.variables.get_mut(name)
     }
     
     /// 変数を取得します。
     /// - `name` - 変数名
-    pub fn get_variable(&self, name: &String) -> Option<&i32> {
+    pub fn get_variable(&self, name: &String) -> Option<&VarType> {
         self.variables.get(name)
     }
 
     /// 変数を作成します。
     /// - `name` - 新しく作成する変数名
     pub fn create_variable(&mut self, name: String){
-        self.variables.insert(name, 0);
+        self.variables.insert(name, VarType::Uninitialized);
     }
 
     /// 文字列を式として解釈します。
     /// * `cmd` - 式として扱う文字列
-    pub fn parse(&mut self, cmd: &String) -> Result<Option<i32>, Box<dyn Error>> {
+    pub fn parse(&mut self, cmd: &String) -> Result<VarType, Box<dyn Error>> {
         self.split_elements(cmd); // 要素単位に分解
         match self.cmds.iter().filter(|a| **a == "(").count().cmp(&self.cmds.iter().filter(|a| **a == ")").count()) {
             std::cmp::Ordering::Less => ret_err!(BracketError::new("(")),
@@ -145,7 +145,7 @@ impl ExprParser {
     }
 
     /// 分割された要素を解釈する関数です。
-    fn parse_middle_phase(&mut self, pointer: &mut usize) -> Result<Option<i32>, Box<dyn Error>> {
+    fn parse_middle_phase(&mut self, pointer: &mut usize) -> Result<VarType, Box<dyn Error>> {
         let mut list: Vec<(String, VecDeque<ElementType>)> = Vec::new();
         let mut monomial_flag: Option<String> = None;
         while *pointer < self.cmds.len() {
@@ -161,24 +161,41 @@ impl ExprParser {
                         }
                         ret_err!(InvalidExpressionError::new("Next of \"let\" keyword must be variable name."))
                     },
-                    "(" => Immediate(self.parse_middle_phase({*pointer += 1; pointer})?.unwrap()),
+                    "(" => Immediate(self.parse_middle_phase({*pointer += 1; pointer})?),
                     _ => match a.parse::<i32>() {
                         Ok(b) => {
                             if monomial_flag.is_some() {
-                                Monomial(monomial_flag.clone().unwrap(), Rc::new(Immediate(b)))
+                                Monomial(monomial_flag.clone().unwrap(), Rc::new(Immediate(VarType::Integer(b))))
                             } else {
-                                Immediate(b)
+                                Immediate(VarType::Integer(b))
                             }
                         },
                         Err(_) => match self.get_variable(&a.to_string()) {
-                            Some(_) => Variable(a.clone()),
+                            Some(_) => {
+                                if monomial_flag.is_some() {
+                                    Monomial(monomial_flag.clone().unwrap(), Rc::new(Variable(a.clone())))
+                                } else {
+                                    Variable(a.clone())
+                                }
+                            },
                             None => if Self::is_monomial(&a) {
                                 monomial_flag = Some(a.to_string());
                                 continue;
                             } else {
                                 match Self::get_priority(&a) {
                                     Some(_) => ret_err!(InvalidExpressionError::new("Illegal operator.")),
-                                    None => ret_err!(VariableNotFoundError::new(a.clone())),
+                                    None => {
+                                        let mut t = a.chars();
+                                        if t.next() == Some('"') && t.last() == Some('"') {
+                                            if monomial_flag.is_some() {
+                                                Monomial(monomial_flag.clone().unwrap(), Rc::new(Immediate(VarType::String(a.clone()))))
+                                            } else {
+                                                Immediate(VarType::String(a.clone()))
+                                            }
+                                        } else {
+                                            ret_err!(VariableNotFoundError::new(a.clone()))
+                                        }
+                                    },
                                 }
                             }
                         },
@@ -197,9 +214,10 @@ impl ExprParser {
                         }
                         if upcoming != &a.0 && Self::get_priority(upcoming) >= Self::get_priority(&a.0) {
                             a.1.push_back(n);
-                            match self.try_calculate_all(a)? {
-                                Some(b) => list.push(({*pointer += 1; self.cmds.get(*pointer - 1)}.unwrap().clone(), vec_deque!(Immediate(b)))),
-                                None => ret_err!(OperationError::operate_with_void()),
+                            let tmp = self.try_calculate_all(a)?;
+                            match tmp {
+                                VarType::Uninitialized | VarType::Void => ret_err!(OperationError::operate_with_void()),
+                                _ => list.push(({*pointer += 1; self.cmds.get(*pointer - 1)}.unwrap().clone(), vec_deque![Immediate(tmp)])),
                             }
                         } else {
                             list.push(a);
@@ -214,7 +232,7 @@ impl ExprParser {
             } else {
                 match {*pointer += 1;self.cmds.get(*pointer - 1)} {
                     Some(a) => list.push((a.clone(), vec_deque!(n))),
-                    None => return Ok(n.to_i32(self)),
+                    None => return n.to_vartype(self),
                 }
             }
         }
@@ -228,12 +246,12 @@ impl ExprParser {
                     match num {
                         Ok(a) => {
                             match a {
-                                Some(b) => {
+                                VarType::Uninitialized | VarType::Void => ret_err!(OperationError::operate_with_void()),
+                                _ => {
                                     let mut c = list.pop().unwrap();
-                                    c.1.push_back(Immediate(b));
+                                    c.1.push_back(Immediate(a));
                                     num = self.try_calculate_all(c);
                                 },
-                                None => ret_err!(OperationError::operate_with_void()),
                             }
                         },
                         Err(e) => return Err(e),
@@ -273,61 +291,61 @@ impl ExprParser {
         return ops.contains(&op);
     }
 
-    fn try_calculate_all(&mut self, mut data: (String, VecDeque<ElementType>)) -> Result<Option<i32>, Box<dyn Error>> {
+    fn try_calculate_all(&mut self, mut data: (String, VecDeque<ElementType>)) -> Result<VarType, Box<dyn Error>> {
         if data.0 != "=" {
             let mut num = data.1.pop_front().unwrap();
             while !data.1.is_empty() {
                 let d = data.1.pop_front().unwrap();
                 num = match self.calculate_binomial(&data.0, num, d)? {
-                    Some(a) => Immediate(a),
-                    None => return Ok(None),
+                    VarType::Integer(i) => Immediate(VarType::Integer(i)),
+                    _ => return Ok(VarType::Void),
                 };
             }
-            return Ok(num.to_i32(self));
+            return num.to_vartype(self);
         } else {
             let mut num = data.1.pop_back().unwrap();
             while !data.1.is_empty() {
                 let d = data.1.pop_back().unwrap();
                 num = match self.calculate_binomial(&data.0, d, num)? {
-                    Some(a) => Immediate(a),
-                    None => return Ok(None),
+                    VarType::Integer(i) => Immediate(VarType::Integer(i)),
+                    _ => return Ok(VarType::Void)
                 }
             }
-            return Ok(num.to_i32(self));
+            return num.to_vartype(self);
         }
     }
 
-    fn calculate_binomial(&mut self, op: &str, left: ElementType, right: ElementType) -> Result<Option<i32>, Box<dyn Error>> {
+    fn calculate_binomial(&mut self, op: &str, left: ElementType, right: ElementType) -> Result<VarType, Box<dyn Error>> {
         match op {
-            "+" => left.op_some(self, right, |a, b| a + b).map(|a| Some(a)),
-            "-" => left.op_some(self, right, |a, b| a - b).map(|a| Some(a)),
-            "*" => left.op_some(self, right, |a, b| a * b).map(|a| Some(a)),
-            "/" => left.op_some(self, right, |a, b| a / b).map(|a| Some(a)),
-            "%" => left.op_some(self, right, |a, b| a % b).map(|a| Some(a)),
-            "|" => left.op_some(self, right, |a, b| a | b).map(|a| Some(a)),
-            "&" => left.op_some(self, right, |a, b| a & b).map(|a| Some(a)),
-            "^" => left.op_some(self, right, |a, b| a ^ b).map(|a| Some(a)),
-            ">>" => left.op_some(self, right, |a, b| a >> b).map(|a| Some(a)),
-            "<<" => left.op_some(self, right, |a, b| a << b).map(|a| Some(a)),
-            "==" => left.op_some(self, right, |a, b| if a == b {1} else {0}).map(|a| Some(a)),
-            "!=" => left.op_some(self, right, |a, b| if a != b {1} else {0}).map(|a| Some(a)),
-            ">" => left.op_some(self, right, |a, b| if a > b {1} else {0}).map(|a| Some(a)),
-            "<" => left.op_some(self, right, |a, b| if a < b {1} else {0}).map(|a| Some(a)),
-            ">=" => left.op_some(self, right, |a, b| if a >= b {1} else {0}).map(|a| Some(a)),
-            "<=" => left.op_some(self, right, |a, b| if a <= b {1} else {0}).map(|a| Some(a)),
-            "&&" => left.op_some(self, right, |a, b| if a != 0 && b != 0 {1} else {0}).map(|a| Some(a)),
-            "||" => left.op_some(self, right, |a, b| if a != 0 || b != 0 {1} else {0}).map(|a| Some(a)),
+            "+" => left.op_num(self, right, |a, b| a + b),
+            "-" => left.op_num(self, right, |a, b| a - b),
+            "*" => left.op_num(self, right, |a, b| a * b),
+            "/" => left.op_num(self, right, |a, b| a / b),
+            "%" => left.op_num(self, right, |a, b| a % b),
+            "|" => left.op_num(self, right, |a, b| a | b),
+            "&" => left.op_num(self, right, |a, b| a & b),
+            "^" => left.op_num(self, right, |a, b| a ^ b),
+            ">>" => left.op_num(self, right, |a, b| a >> b),
+            "<<" => left.op_num(self, right, |a, b| a << b),
+            "==" => left.op_num(self, right, |a, b| if a == b {1} else {0}),
+            "!=" => left.op_num(self, right, |a, b| if a != b {1} else {0}),
+            ">" => left.op_num(self, right, |a, b| if a > b {1} else {0}),
+            "<" => left.op_num(self, right, |a, b| if a < b {1} else {0}),
+            ">=" => left.op_num(self, right, |a, b| if a >= b {1} else {0}),
+            "<=" => left.op_num(self, right, |a, b| if a <= b {1} else {0}),
+            "&&" => left.op_num(self, right, |a, b| if a != 0 && b != 0 {1} else {0}),
+            "||" => left.op_num(self, right, |a, b| if a != 0 || b != 0 {1} else {0}),
             "=" => left.op_let(self, right, |a, b| *a = b),
-            "+=" => left.op_let(self, right, |a, b| *a += b),
-            "-=" => left.op_let(self, right, |a, b| *a -= b),
-            "*=" => left.op_let(self, right, |a, b| *a *= b),
-            "/=" => left.op_let(self, right, |a, b| *a /= b),
-            "%=" => left.op_let(self, right, |a, b| *a %= b),
-            "|=" => left.op_let(self, right, |a, b| *a |= b),
-            "&=" => left.op_let(self, right, |a, b| *a &= b),
-            "^=" => left.op_let(self, right, |a, b| *a ^= b),
-            ">>=" => left.op_let(self, right, |a, b| *a >>= b),
-            "<<=" => left.op_let(self, right, |a, b| *a <<= b),
+            // "+=" => left.op_let(self, right, |a, b| *a += b),
+            // "-=" => left.op_let(self, right, |a, b| *a -= b),
+            // "*=" => left.op_let(self, right, |a, b| *a *= b),
+            // "/=" => left.op_let(self, right, |a, b| *a /= b),
+            // "%=" => left.op_let(self, right, |a, b| *a %= b),
+            // "|=" => left.op_let(self, right, |a, b| *a |= b),
+            // "&=" => left.op_let(self, right, |a, b| *a &= b),
+            // "^=" => left.op_let(self, right, |a, b| *a ^= b),
+            // ">>=" => left.op_let(self, right, |a, b| *a >>= b),
+            // "<<=" => left.op_let(self, right, |a, b| *a <<= b),
             _ => unreachable!(),
         }
     }
@@ -380,26 +398,51 @@ impl CharType {
 #[derive(Debug, Clone)]
 pub enum ElementType {
     Variable(String),
-    Immediate(i32),
+    Immediate(VarType),
     Monomial(String, Rc<ElementType>)
 }
 
 impl ElementType {
+    fn get(&self) -> &Self {self}
     /// 数値へ変換します。
-    fn to_i32(&self, expr: &ExprParser) -> Option<i32> {
+    fn to_vartype(&self, expr: &ExprParser) -> Result<VarType, Box<dyn Error>> {
         match self {
-            ElementType::Variable(s) => expr.get_variable(s).map(|a| *a),
-            ElementType::Immediate(i) => Some(*i),
+            ElementType::Variable(s) => match expr.get_variable(s) {
+                Some(a) => Ok(a.clone()),
+                None => Ok(VarType::Void),
+            },
+            ElementType::Immediate(i) => Ok(i.clone()),
             ElementType::Monomial(s, e) => {
-                e.to_i32(expr).map(|a| match s.as_str() {
-                    "+" => a,
-                    "-" => -a,
-                    "~" => !a,
-                    "!" => if a == 0 {1} else {0},
-                    // "*" => 
-                    // "&" => 
-                    _ => unreachable!()
-                })
+                if s == "&" {
+                    match e.get() {
+                        Variable(v) => Ok(VarType::Pointer(v.clone())),
+                        _ => ret_err!(OperationError::invalid_dereference())
+                    }
+                } else {
+                    match e.to_vartype(expr)? {
+                        VarType::Uninitialized | VarType::Void => ret_err!(OperationError::operate_with_void()),
+                        VarType::Integer(i) => {
+                            match s.as_str() {
+                                    "+" => Ok(VarType::Integer(i)),
+                                    "-" => Ok(VarType::Integer(-i)),
+                                    "~" => Ok(VarType::Integer(!i)),
+                                    "!" => Ok(VarType::Integer(if i == 0 {1} else {0})),
+                                    _ => unreachable!()
+                            }
+                        },
+                        VarType::String(_) => todo!(),
+                        VarType::Pointer(p) => {
+                            if s == "*" {
+                                match expr.get_variable(&p) {
+                                    Some(i) => Ok(i.clone()),
+                                    None => Ok(VarType::Void),
+                                }
+                            } else {
+                                unreachable!()
+                            }
+                        },
+                    }
+                }
             }
         }
     }
@@ -408,13 +451,13 @@ impl ElementType {
     /// - `expr` - 処理を呼び出すパーサのインスタンス
     /// - `right` - 右辺に来る `ElementType` 構造体
     /// - `op` - 具体的な処理内容を記述するクロージャ
-    fn op_some<F>(self, expr: &ExprParser, other: ElementType, op: F) -> Result<i32, Box<dyn Error>>
+    fn op_num<F>(self, expr: &ExprParser, other: ElementType, op: F) -> Result<VarType, Box<dyn Error>>
     where
         F: Fn(i32, i32) -> i32,
     {
-        if let Some(left) = self.to_i32(expr) {
-            if let Some(right) = other.to_i32(expr) {
-                return Ok(op(left, right));
+        if let VarType::Integer(left) = self.to_vartype(expr)? {
+            if let VarType::Integer(right) = other.to_vartype(expr)? {
+                return Ok(VarType::Integer(op(left, right)));
             }
         }
         ret_err!(InvalidExpressionError::new("Cannot operate with void"))
@@ -424,21 +467,16 @@ impl ElementType {
     /// - `expr` - 処理を呼び出すパーサのインスタンス
     /// - `right` - 右辺に来る `ElementType` 構造体
     /// - `op` - 具体的な処理内容を記述するクロージャ
-    fn op_let<F>(self, expr: &mut ExprParser, right: ElementType, op: F) -> Result<Option<i32>, Box<dyn Error>>
+    fn op_let<F>(self, expr: &mut ExprParser, right: ElementType, op: F) -> Result<VarType, Box<dyn Error>>
     where
-        F: Fn(&mut i32, i32),
+        F: Fn(&mut VarType, VarType),
     {
         if let Variable(v) = self {
-            let c = right.to_i32(expr);
+            let c = right.to_vartype(expr);
             match expr.get_variable_mut(&v) {
                 Some(a) => {
-                    match c {
-                        Some(b) => {
-                            op(a, b);
-                            return Ok(None);
-                        },
-                        None => todo!(),
-                    }
+                    op(a, c?);
+                    return Ok(VarType::Void);
                 },
                 None => {ret_err!(VariableNotFoundError::new(v))},
             }
@@ -446,4 +484,22 @@ impl ElementType {
         ret_err!(InvalidExpressionError::new("The left-hand must be variable."));
     }
 
+}
+
+#[derive(Debug, Clone)]
+pub enum VarType {
+    Uninitialized,
+    Void,
+    Integer(i32),
+    String(String),
+    Pointer(String),
+}
+
+impl VarType {
+    fn is_empty(&self) -> bool {
+        match self {
+            VarType::Uninitialized | VarType::Void => true,
+            _ => false
+        }
+    }
 }
